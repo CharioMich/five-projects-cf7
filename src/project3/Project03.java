1200a291cfcb6b70b7e8da10ef911cfff759f708
Ch. Michalakis <mpampisb@gmail.com>
2024-12-23 13:53:34 +0200

    project3 commit

---
+++ src/project3/Project03.java
@@ -0,0 +1,42 @@
+package project3;
+
+import java.io.*;
+
+public class Project03 {
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[128][2];
+        String inFile = "C:/temp/lorem.txt";
+
+        try (FileReader fileReader = new FileReader(inFile)) {
+            int i;
+            while ((i = fileReader.read()) != -1) {
+                if (i == 32 || i == 13 || i == 10 || i == 9) {
+                    continue;
+                }
+                else if (arr[i][0] == 0) {
+                    arr[i][0] = i;
+                    arr[i][1]++;
+                }
+                else {
+                    arr[i][1]++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        printStats(arr);
+
+    }
+
+    public static void printStats (int[][] arr) {
+        for (int[] ch : arr) {
+            if (ch[0] != 0) {
+                String ending = ch[1] == 1 ? " time." : " times.";
+                System.out.println("Character: " + "'" + (char) ch[0] + "'," + " occurred " + ch[1] + ending);
+            }
+        }
+    }
+}
